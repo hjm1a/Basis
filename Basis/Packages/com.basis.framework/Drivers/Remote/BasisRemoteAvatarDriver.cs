fb8aa837eae9e86170e572e36f8487b958a1e0ff
dooly <doolanl208@gmail.com>
2025-09-18 07:40:19 +1000

    disabled once again logs

--- Basis/Packages/com.basis.framework/Drivers/Remote/BasisRemoteAvatarDriver.cs
+++ Basis/Packages/com.basis.framework/Drivers/Remote/BasisRemoteAvatarDriver.cs
@@ -112,7 +112,7 @@ namespace Basis.Scripts.Drivers
         public RuntimeAnimatorController SavedruntimeAnimatorController;
         public void PutAvatarIntoTPose()
         {
-            BasisDebug.Log("PutAvatarIntoTPose", BasisDebug.LogTag.Avatar);
+          //  BasisDebug.Log("PutAvatarIntoTPose", BasisDebug.LogTag.Avatar);
             CurrentlyTposing = true;
             if (SavedruntimeAnimatorController == null)
             {
@@ -135,7 +135,7 @@ namespace Basis.Scripts.Drivers
 
         public void ResetAvatarAnimator()
         {
-            BasisDebug.Log("ResetAvatarAnimator", BasisDebug.LogTag.Avatar);
+           // BasisDebug.Log("ResetAvatarAnimator", BasisDebug.LogTag.Avatar);
             Player.BasisAvatar.Animator.runtimeAnimatorController = SavedruntimeAnimatorController;
             SavedruntimeAnimatorController = null;
             CurrentlyTposing = false;
